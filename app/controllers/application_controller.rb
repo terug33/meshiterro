e3df286a0113ab738dff93c0dfb02afaf8b8e12d
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-05-20 13:36:09 +0000

    4-7-2136

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   # privateは記述をしたコントローラ内でしか参照できません。
   # 一方、protectedは呼び出された他のコントローラからも参照することができます。
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/top' => "homes#top"
   root "homes#top"
+
+  get 'homes/about' => 'homes#about', as: :about
   
 end
